399bd4b16a6c9340a7552a6cc563cc6c70176058
smallinvestor <jumpupdnbzone@gmail.com>
2024-11-16 02:38:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         <a href='https://t.me/skicatportal' target='_blank'>        <img
           className='soc'
           src={tg}
-          alt="Ski Mask Catt"
+          alt="Ski Mask Cat"
         /></a>
 
         <img
